d928c94c803d431edeed409df8ee4ce79536ab08
Patrick fitzsimons <patrickfitzs17@hotmail.com>
2025-11-17 23:34:01 +0000

    updated product service with findMany and findOne async functionality

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from "../prisma/prisma/prisma.service"; // straight up to Prisma schema (client)
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    //constructor
+    constructor(private readonly prisma: PrismaService) {}
+    // method to get all products (non deleted), sorted by name, includes the category
+    async findAll() {
+        return this.prisma.product.findMany({
+            where: { deletedAt: null }, // ignore deleted products
+            orderBy: { name: 'asc'},
+            include: {
+                category: true, // to join the Category row so the prisma client gets profuct plus the ategory together
+            },
+        });
+    }
+
+    // findOne, to return a single product by its id.
+
+}
